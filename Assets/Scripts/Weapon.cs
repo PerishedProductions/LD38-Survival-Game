d603b1ffe6ffc1dceed29139b5fb11d2aa514805
Mikkel Mouridsen <mikkelmouridsen@me.com>
2017-04-22 23:04:28 +0200

    Treewes

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public int health = 100;
+
+	void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            health -= 1;
+        }
+
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
