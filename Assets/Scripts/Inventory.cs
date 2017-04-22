66147e021c53a55c21c3cdaa66ad421df192218b
ImTruxaHD <imtruxa@gmail.com>
2017-04-22 20:09:32 +0200

    Inventory
    Added basics for inv

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public List<Item> items;
+
+	// Use this for initialization
+	void Start () {
+
+        items = new List<Item>();
+
+        AddItem(new Item ());
+	}
+
+    public void AddItem (Item itemToAdd)
+    {
+        items.Add(itemToAdd);
+    }
+}
