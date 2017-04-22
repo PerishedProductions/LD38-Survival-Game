de173c2527b9f913a47e785ea497a9526857239b
Mikkel Mouridsen <mikkelmouridsen@me.com>
2017-04-22 20:57:00 +0200

    Added tree chopping mechanic

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour {
+
+    public GameObject[] drops;
+    public int health = 100;
+    
+    void Update()
+    {
+        if (health <= 0)
+        {
+            for (int i = 0; i < drops.Length; i++)
+            {
+                Instantiate(drops[i], this.transform.position, Quaternion.identity);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    //Checks if player enters and hits object
+	void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                health -= 10;
+                Debug.Log(health);
+            }
+        }
+    }
+
+}
